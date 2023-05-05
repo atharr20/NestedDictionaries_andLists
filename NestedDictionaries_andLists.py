50623efa67de9abdeb691d81054d5e0df4b2895d
Mohammed Athar <atharr20@gmail.com>
2023-05-05 14:55:05 -0400

    first commit

---
+++ NestedDictionaries_andLists.py
@@ -0,0 +1,72 @@
+#1. Update Values in Dictionaries and Lists
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0]=15
+print(x)
+
+students[0]['last_name']='Bryant'
+print(students)
+
+sports_directory['soccer'][0]='Andres'
+print(sports_directory)
+
+z[0]['y']=30
+print(z)
+
+
+#2. Iterate Through a List of Dictionaries
+
+iterateDictionary = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+for students in range(len(iterateDictionary)):
+  for key in iterateDictionary[students]:
+    print(iterateDictionary[students])
+
+
+# 3. Get Values From a List of Dictionaries
+iterateDictionary2 = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+for students2 in iterateDictionary2:
+    print(students2['first_name'])
+    print(students2['last_name'])
+
+
+printInfo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+for key in printInfo:   
+    print(f'{len(printInfo[key])} {key}')
+    for value in printInfo[key]:
+       print(value)
+
+
